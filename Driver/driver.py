196bbee40a2e8c57eb671e888be97e09d3d6ffd9
Boris Skip <boris.v.skip@gmail.com>
2020-01-02 17:00:22 +0200

    Init

--- Driver/driver.py
+++ Driver/driver.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import IEDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
 class Driver:
@@ -19,12 +21,34 @@ class Driver:
     def __init__(self, browser):
         self.browser = browser
 
+    @property
     def set_browser(self):
+
         if self.browser.lower() == "firefox":
-            return webdriver.Firefox(executable_path=GeckoDriverManager().install())
+            options = FirefoxOptions()
+            options.headless = True
+            return webdriver.Firefox(executable_path=GeckoDriverManager().install(), firefox_options=options)
+
         elif self.browser.lower() == "chrome":
-            return webdriver.Chrome(ChromeDriverManager().install())
+            options = Options()
+            options.headless = True
+            # options.add_argument("headless")
+            '''from selenium import webdriver
+               from selenium.webdriver.chrome.options import Options
+               options = Options()
+               options.add_argument("no-sandbox")
+               options.add_argument("start-maximized")
+               options.add_argument("window-size=1900,1080"); 
+               driver = webdriver.Chrome(chrome_options=options, executable_path="/usr/bin/chromedriver")'''
+            # options.headless = True
+            # options.add_argument('--headless')
+            # options.add_argument('--disable-gpu')
+            # options.add_argument("--no-sandbox")
+            # options.add_argument("window-size=1400,2100")  # Linux should be activate
+            return webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+
         elif self.browser.lower() == "ie":
-            return webdriver.Ie(IEDriverManager().install())
+            return webdriver.Ie(IEDriverManager().install(), ie_options=options)
+
         else:
             raise Exception("Selected browser not supported")
